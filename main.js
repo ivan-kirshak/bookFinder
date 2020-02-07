629cd04aea8a803832746dddba2c0e07cfa701d3
Ivan Kirshak <i.kirshak@gmail.com>
2020-02-07 16:56:09 +0200

    update of functionality and slightly tidy up layout

--- main.js
+++ main.js
@@ -13,19 +13,43 @@ function lookForBook() {
         XHR.addEventListener("readystatechange", function () {
             if (XHR.status === 200 && XHR.readyState === 4) {
                 let result = JSON.parse(XHR.responseText);
-                //console.log(result);
                 for (let i = 0; i < result.items.length; i++) {
-                    //console.log(result.items[i].volumeInfo.title, result.items[i].volumeInfo.authors.join(','));
-                    //console.log(result.items[i].saleInfo.listPrice.amount);
                     let item = document.createElement("li");
+                    if (result.items[i].saleInfo.saleability === "NOT_FOR_SALE") {
                     item.innerHTML = `<div class="bookItem">
-                <div class="thumbnail"><img src="${result.items[i].volumeInfo.imageLinks.thumbnail}"></div>
+                    <div class="thumbnail"><img src="${result.items[i].volumeInfo.imageLinks.thumbnail}"></div>
                     <div class="main-info">
                     <h1 class="mobheading">${result.items[i].volumeInfo.title}</h1>
-                    <h2 class="subheading">by ${result.items[i].volumeInfo.authors.join(',')}</h2>
+                    <h2 class="subheading">by ${result.items[i].volumeInfo.authors}</h2>
                     <p class="description"><i>${result.items[i].volumeInfo.description}</i></p>
-                </div>
-                </div>`;
+                    <a href="${result.items[i].accessInfo.webReaderLink}" target="_blank" class="link">view sample > </a>
+                    </div>
+                    </div>`;
+                    } else if (result.items[i].saleInfo.saleability === "FOR_SALE") {
+                        item.innerHTML = `<div class="bookItem">
+                    <div class="thumbnail"><img src="${result.items[i].volumeInfo.imageLinks.thumbnail}"></div>
+                    <div class="main-info">
+                    <h1 class="mobheading">${result.items[i].volumeInfo.title}</h1>
+                    <h2 class="subheading">by ${result.items[i].volumeInfo.authors}</h2>
+                    <p class="description"><i>${result.items[i].volumeInfo.description}</i></p>
+                    <a href="${result.items[i].accessInfo.webReaderLink}" target="_blank" class="link">view sample > </a>
+                    <a href="${result.items[i].saleInfo.buyLink}" target="_blank" class="buylink">buy 
+                    (from ${result.items[i].saleInfo.listPrice.amount}
+                    ${result.items[i].saleInfo.listPrice.currencyCode}) > </a>
+                    </div>
+                    </div>`;
+                    } else if (result.items[i].saleInfo.saleability === "FREE") {
+                        item.innerHTML = `<div class="bookItem">
+                    <div class="thumbnail"><img src="${result.items[i].volumeInfo.imageLinks.thumbnail}"></div>
+                    <div class="main-info">
+                    <h1 class="mobheading">${result.items[i].volumeInfo.title}</h1>
+                    <h2 class="subheading">by ${result.items[i].volumeInfo.authors}</h2>
+                    <p class="description"><i>${result.items[i].volumeInfo.description}</i></p>
+                    <a href="${result.items[i].accessInfo.webReaderLink}" target="_blank" class="link">view sample > </a>
+                    <a href="${result.items[i].saleInfo.buyLink}" target="_blank" class="freelink">get free eBook > </a>
+                    </div>
+                    </div>`;
+                    }
                     bookFind.appendChild(item);
                 }
             }
